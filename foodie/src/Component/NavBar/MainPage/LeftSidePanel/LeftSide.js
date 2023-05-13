11a14390e7bd82e20448a24923a9d06ffb7ce7bd
Navindu Praveen <talktopra@gmail.com>
2023-05-13 23:07:07 +0530

    change to dynamically show data on left hand side panel

--- foodie/src/Component/NavBar/MainPage/LeftSidePanel/LeftSide.js
+++ foodie/src/Component/NavBar/MainPage/LeftSidePanel/LeftSide.js
@@ -4,21 +4,68 @@ import ImageLayout from '../ImageLayout';
 import groups from "../../../../images/groups.png";
 import memories from "../../../../images/memories.png";
 import messengerKids from "../../../../images/messengerkids.png";
-import ads from "../../../../images/ads.png"
+import ads from "../../../../images/ads.png";
+import adsmanager from "../../../../images/admanager.png";
+import business from "../../../../images/business.png";
 
 class LeftSide extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            data :[]
+         }
+    }
+
+    getData=()=>{ //FAke JSON RESPONSE
+        let jsondata = [
+            {
+                "image": JSON.parse(localStorage.getItem("user")).userImage,
+                "text": JSON.parse(localStorage.getItem("user")).userName
+            },
+            {
+                "image": groups,
+                "text":"Friends"
+            },
+            {
+                "image": memories,
+                "text":"Memories"
+            },
+            {
+                "image": messengerKids,
+                "text":"Messenger"
+            },
+            {
+                "image": ads,
+                "text":"Ad Center"
+            },
+            {
+                "image": adsmanager,
+                "text":"Ads Manager"
+            },
+           
+            {
+                "image": business,
+                "text":"Business Manager"
+            }    
+        ];
+        this.setState({data : jsondata});
+    }
+
+    componentDidMount() {
+        this.getData();
     }
     
     render() { 
-        return ( <div>
-            <ImageLayout image={groups} text="Friends"/>
-            <ImageLayout image={memories} text="Memories"/>
-            <ImageLayout image={messengerKids} text="Messenger"/>
-            <ImageLayout image={ads} text="Ads"/>
-        </div> );
+        return ( 
+         <div>
+             {
+                 this.state.data.map( (item) =>(
+                        <ImageLayout image={item.image} text={item.text} />
+                 ))
+             }
+
+         </div> 
+        );
     }
 }
  
