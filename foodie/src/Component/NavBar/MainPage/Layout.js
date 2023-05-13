e8f9d5f56b81bfd26586641d19d06ccd4bb069d3
Navindu Praveen <talktopra@gmail.com>
2023-05-13 23:47:01 +0530

    start setting status panel

--- foodie/src/Component/NavBar/MainPage/Layout.js
+++ foodie/src/Component/NavBar/MainPage/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./Mainpage.css";
 import { Grid } from '@material-ui/core';
 import LeftSide from './LeftSidePanel/LeftSide';
+import StatusBar from './StatusBar/Status';
 
 class Layout extends Component {
     constructor(props) {
@@ -16,7 +17,7 @@ class Layout extends Component {
                     <LeftSide />
                 </Grid>
                 <Grid item xs={6}>
-                    midle
+                    <StatusBar />
                 </Grid>
                 <Grid item xs={3}>
                     right
---
+++ foodie/src/Component/NavBar/MainPage/StatusBar/Status.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class StatusBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    
+    render() { 
+        return ( 
+            <div>
+                Status Panel
+            </div>
+         );
+    }
+}
+ 
+export default StatusBar;
