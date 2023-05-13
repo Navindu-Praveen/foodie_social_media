96728f1dc806bf235ccfb9d374ead7a7e5a49756
Navindu Praveen <talktopra@gmail.com>
2023-05-13 21:18:02 +0530

    create 3 main grids to setup foodie page

--- foodie/src/Component/NavBar/MainPage/Layout.js
+++ foodie/src/Component/NavBar/MainPage/Layout.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import "./Mainpage.css";
+import { Grid } from '@material-ui/core';
 
 class Layout extends Component {
     constructor(props) {
         super(props);
+        this.state ={}
     }
-    state = {  }
     render() { 
-        return ( <div>
-            Main Page
+        return ( 
+        <div className="mainpage__container">
+            <Grid container>
+                <Grid item xs={3}>
+                    left
+                </Grid>
+                <Grid item xs={6}>
+                    midle
+                </Grid>
+                <Grid item xs={3}>
+                    right
+                </Grid>
+                </Grid>
         </div> );
     }
 }
