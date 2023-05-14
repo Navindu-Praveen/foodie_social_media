3135519c83f355bd8330dc8072c48f4169a05e50
Navindu Praveen <talktopra@gmail.com>
2023-05-14 09:23:51 +0530

    left hand side panel create

--- foodie/src/Component/NavBar/MainPage/RighrSidePanel/RightSide.js
+++ foodie/src/Component/NavBar/MainPage/RighrSidePanel/RightSide.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
 import "./RightSide.css";
+import ImageLayout from '../ImageLayout';
+import groups from "../../../../images/groups.png";
+import memories from "../../../../images/memories.png";
+import messengerKids from "../../../../images/messengerkids.png";
+import ads from "../../../../images/ads.png";
+import adsmanager from "../../../../images/admanager.png";
+import business from "../../../../images/business.png";
 
 class RightSide extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            data :[]
+        }
+    }
+    getData=()=>{ //FAke JSON RESPONSE
+        let jsondata = [
+            
+            {
+                "image": groups,
+                "text":"Friends"
+            },
+            {
+                "image": memories,
+                "text":"Memories"
+            },
+            {
+                "image": messengerKids,
+                "text":"Messenger"
+            },
+            {
+                "image": ads,
+                "text":"Ad Center"
+            },
+            {
+                "image": adsmanager,
+                "text":"Ads Manager"
+            },
+           
+            {
+                "image": business,
+                "text":"Business Manager"
+            }    
+        ];
+        this.setState({data : jsondata});
+    }
+
+    componentDidMount() {
+        this.getData();
     }
     
     render() { 
@@ -15,6 +59,11 @@ class RightSide extends Component {
                 </div>
 
                 <div className="rightside__content">
+             {
+                 this.state.data.map( (item) =>(
+                        <ImageLayout image={item.image} text={item.text} />
+                 ))
+             }
 
                 </div>
             </div>
